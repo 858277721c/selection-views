e70b9724714a1b547c155a6fd59e2ec872d8094d
zjun <565061763@qq.com>
2019-06-13 15:30:51 +0800

    FTabUnderline

--- lib/src/main/java/com/sd/lib/selection/views/FTabUnderline.java
+++ lib/src/main/java/com/sd/lib/selection/views/FTabUnderline.java
@@ -2,30 +2,21 @@ package com.sd.lib.selection.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class FTabUnderline extends FrameLayout
+public class FTabUnderline extends LinearLayout
 {
-    public FTabUnderline(Context context)
-    {
-        super(context);
-        init();
-    }
+    public TextView tv_text;
+    public View view_underline;
 
     public FTabUnderline(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-        init();
-    }
-
-    public TextView tv_text;
-    public View view_underline;
-
-    protected void init()
-    {
+        setGravity(Gravity.CENTER);
         LayoutInflater.from(getContext()).inflate(R.layout.lib_selection_views_view_tab_underline, this, true);
         tv_text = findViewById(R.id.tv_text);
         view_underline = findViewById(R.id.view_underline);
